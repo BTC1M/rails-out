089b97114c584d83115cc31de0bfaaf1962d6be1
Franck Robin <franck.robin@hotmail.fr>
2019-08-20 17:47:29 +0200

    premieres views, modifs controllers et suppression routes inutiles

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,15 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :places
-  resources :artists
+  #resources :places
+  resources :artists, only: [:index, :show]
 
-  resources :events do
+  resources :events, only: [:index, :show] do
     resources :user_participations, only: [:new, :create, :destroy]
     resources :artist_participations, only: [:new, :create, :destroy]
   end
 
   get "/dashboard", to:"pages#dashboard"
+  get "/discover", to:"pages#discover"
 
 end
