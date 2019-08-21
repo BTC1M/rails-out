366600ce067e634be26f916721054f063bfd35b0
Franck Robin <franck.robin@hotmail.fr>
2019-08-21 17:14:18 +0200

    rectif erreur start_time

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,7 +6,7 @@ class EventsController < ApplicationController
       {
         lat: place.latitude,
         lng: place.longitude,
-        infoWindow: render_to_string(partial: "info_window", locals: { event: place.events.where("start_time > ?", DateTime.now).order(start_time: :asc).first}),
+        infoWindow: render_to_string(partial: "info_window", locals: { event: place.events.where("date > ?", Date.current).order(start_time: :asc).first}),
         image_url: helpers.asset_url('red-dot.png')
       }
     end
