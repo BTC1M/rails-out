eb76e7b7eb6cd967f0e9d4a1a832cd3e21ad9f64
Franck Robin <franck.robin@hotmail.fr>
2019-08-25 20:22:11 +0200

    Correction de bugs d'imcompatibilité des images pour la seed + autres bugs

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,7 +6,7 @@ class EventsController < ApplicationController
       {
         lat: place.latitude,
         lng: place.longitude,
-        infoWindow: render_to_string(partial: "info_window", locals: { event: place.events.where("date > ?", Date.current).order(start_time: :asc).first}),
+        infoWindow: render_to_string(partial: "info_window", locals: { event: place.events.where("date >= ?", Date.current).order(start_time: :asc).first}),
         image_url: helpers.asset_url('point-grey.png')
       }
     end
