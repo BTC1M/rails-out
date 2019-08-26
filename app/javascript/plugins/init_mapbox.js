0fbc6c1f027ee06b6aa87053083256532285d41c
remi bongiovanni <remi.bongiovanni@gmail.com>
2019-08-26 15:41:54 +0200

    presquefini

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -23,22 +23,61 @@ const initMapbox = () => {
       const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
 
       // Create a HTML element for your custom marker
-      const element = document.createElement('div');
+
+     const styleDefaultMarker = (element) => {
       element.className = 'marker';
       element.style.backgroundSize = 'contain';
       element.style.width = '15px';
       element.style.height = '15px';
-      element.style.boxShadow = "0px 0px 12px 1px white"
-      element.style.borderRadius = "100px"
-      element.style.border ="1px solid white"
+      element.style.boxShadow = "0px 0px 12px 1px white";
+      element.style.borderRadius = "100px";
+      element.style.border ="1px solid white";
+      element.style.backgroundColor = "#4d4d4d";
+              element.style.position = "absolute";
+
       element.style.backgroundImage = `url('${marker.image_url}')`;
+      }
+
+      const element = document.createElement('div');
+      styleDefaultMarker(element)
 
 
       new mapboxgl.Marker(element)
         .setLngLat([ marker.lng, marker.lat ])
         .setPopup(popup)
         .addTo(map);
+
+
+      
+        element.addEventListener('click', function (e) {
+
+        map.flyTo({center: [ marker.lng, marker.lat ], zoom: 13});
+        element.style.width = '20px';
+        element.style.height = '20px';
+        element.style.boxShadow = "0px 0px 20px 4px white";
+        element.style.border ="3px solid white";
+        element.style.backgroundImage = `url('dot-white.png')`;
+        element.style.backgroundColor = "white";
+        element.style.position = "absolute";
+
+        let markersElements = document.querySelectorAll('.marker') 
+        markersElements.forEach(marker => {
+          if (marker !== element) {
+            styleDefaultMarker(marker)
+          }
+        } )
+      });
+
+
+
+
+      // const zoomMapbox = () => {
+        // marker.addEventListener('click', e => map.flyTo(e.latlng, 15))
+        // map.on('click', 'symbols', function (e) {
+        //       map.flyTo({center: e.features[0].geometry.coordinates});
+      // } 
     });
+
 
     map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken }));
     fitMapToMarkers(map, markers);
